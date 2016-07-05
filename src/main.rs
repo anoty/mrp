01697eb2adc8f8fcd057afe543862e4967e0b29e
Woodane <woodane@github.com>
2016-07-06 00:11:38 +0800

    a case

--- src/main.rs
+++ src/main.rs
@@ -4,8 +4,9 @@ use std::thread;
 use std::io::Read;
 use std::io::Write;
 use std::str;
+![feature(rustc_private)]
+[macro_use] extern crate log;
 extern crate time;
-
 
 fn handle_client(mut stream: TcpStream) {
     let mut buf;
@@ -53,29 +54,25 @@ fn main() {
 
 fn client() {
     let mut stream = TcpStream::connect("127.0.0.1:11211").unwrap();
-    print!("{:?}\n", 12312);
+    debug!("this is a debug {:?}", "message");
 
-    // ignore the Result
-    let st = time::get_time().sec;
-    for _ in 1..1000000 {
-        let b:&[u8] = "set x 0 0 3\r\n123\r\n".as_bytes();
-        let _ = stream.write(b);
-        let mut buf = [0; 512];
-        let _ = match stream.read(&mut buf) {
-            Err(e) => panic!("Got an error: {}", e),
-            Ok(m) => {
-                // println!("{}", m);
-                m
-            }
-        };
-        // let sa = &buf[0..m];
-        //
-        // let s = match str::from_utf8(sa) {
-        //     Ok(v) => v,
-        //     Err(e) => panic!("Invalid UTF-8 sequence: {}", e),
-        // };
+    let b: &[u8] = "set x 0 0 3\r\n123\r\n".as_bytes();
+    let _ = stream.write(b);
+    let mut buf = [0; 512];
+    let m = match stream.read(&mut buf) {
+        Err(e) => panic!("Got an error: {}", e),
+        Ok(m) => {
+            println!("{}", m);
+            m
+        }
+    };
+    let sa = &buf[0..m];
 
-        // println!("{}", s.trim());
-    }
-    println!("{:?}", time::get_time().sec-st);
+    let s = match str::from_utf8(sa) {
+        Ok(v) => v,
+        Err(e) => panic!("Invalid UTF-8 sequence: {}", e),
+    };
+
+    println!("{}", s.trim());
+
 }
